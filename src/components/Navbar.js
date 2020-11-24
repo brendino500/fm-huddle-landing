0903156ec66d8c9a7c605ae1fdbbbb9971694591
brendino500 <brenda.ty@live.com>
2020-11-24 11:18:00 +0000

    navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+} from "@material-ui/core/";
+
+import { ReactComponent as Logo } from "../images/logo.svg";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function Navbar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="transparent" elevation={0}>
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          ></IconButton>
+          <Logo />
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
