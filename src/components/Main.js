326f490fdb003574c5cd960e12190ac122d0be3c
brendino500 <brenda.ty@live.com>
2020-11-24 15:49:51 +0000

    button styled

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,20 +2,40 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Container, Typography, Grid, Button } from "@material-ui/core";
 
+import { ReactComponent as MockupIllustration } from "../images/illustration-mockups.svg";
+
 const useStyles = makeStyles({
   root: {
     margin: 15,
+  },
+  textContainer: {
+    maxWidth: 450,
   },
   title: {
     fontFamily: "Poppins",
     color: "#ffffff",
     fontSize: 34,
     fontWeight: "strong",
+    letterSpacing: 1,
   },
   text: {
     fontFamily: "Poppins",
     color: "#ffffff",
     fontSize: 15,
+  },
+  image: {
+    maxWidth: 450,
+  },
+  button: {
+    color: "#9952FF",
+    borderRadius: 45,
+    margin: 10,
+    width: 200,
+    backgroundColor: "#ffffff",
+  },
+  buttonText: {
+    textTransform: "none",
+    margin: 5,
   },
 });
 
@@ -24,15 +44,22 @@ export default function Main() {
 
   return (
     <Container>
-      <Typography className={classes.title}>
-        <b>Build The Community Your Fans Will Love</b>
-        <Typography className={classes.text}>
-          Huddle re-imagines the way we build communities. You have a voice, but
-          so does your audience. Create connections with your users as you
-          engage in genuine discussion.
-        </Typography>
-        Register
-      </Typography>
+      <Grid container direction="row" justify="center" alignItems="center">
+        <MockupIllustration className={classes.image} />
+        <div className={classes.textContainer}>
+          <Typography className={classes.title}>
+            <b>Build The Community Your Fans Will Love</b>
+            <Typography className={classes.text}>
+              Huddle re-imagines the way we build communities. You have a voice,
+              but so does your audience. Create connections with your users as
+              you engage in genuine discussion.
+            </Typography>
+            <Button className={classes.button} variant="contained">
+              <Typography className={classes.buttonText}>Register</Typography>
+            </Button>
+          </Typography>
+        </div>
+      </Grid>
     </Container>
   );
 }
