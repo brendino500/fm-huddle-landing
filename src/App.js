24b4f6ed8bd4c5c12bb0b163f46bb8c2e7fecde6
brendino500 <brenda.ty@live.com>
2020-11-24 11:22:51 +0000

    footer component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <>
       <Navbar />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import BottomNavigation from "@material-ui/core/BottomNavigation";
+import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
+
+const useStyles = makeStyles({
+  root: {
+    width: 500,
+  },
+});
+
+export default function SimpleBottomNavigation() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  return (
+    <BottomNavigation
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
+      showLabels
+      className={classes.root}
+    >
+      <p class="attribution">
+        Challenge by{" "}
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by
+        <a href="https://brendaty.com" target="_blank" rel="noreferrer">
+          Brenda Ty
+        </a>
+        .
+      </p>
+    </BottomNavigation>
+  );
+}
